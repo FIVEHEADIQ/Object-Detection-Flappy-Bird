e0df4e1f6b40b177a896737c1b3b3f6215b94aed
MrWW06 <156442049+MrWW06@users.noreply.github.com>
2024-01-12 09:34:04 -0500

    Update Yazen.py

--- Yazen.py
+++ Yazen.py
@@ -2,39 +2,24 @@ import sys
 import pygame
 from pygame.locals import QUIT
 import os
-import jack
-pygame.init()
-DISPLAYSURF = pygame.display.set_mode((720, 480))
-
-image_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'images')  
-background_path = os.path.join(image_folder, 'ground.jpg')
-#background.jpg  # Replace with your actual image path
-background = pygame.image.load(background_path).convert()
-background = pygame.transform.smoothscale(background, DISPLAYSURF.get_size())
-bg_width = background.get_width()
-x_pos = 0
-clock = pygame.time.Clock()
-jack = jack.Movement(720,480)
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-
-    # Blit the background image
-    DISPLAYSURF.blit(background, (x_pos, 0))
-    DISPLAYSURF.blit(background, (x_pos + bg_width,0))
-
-    x_pos -= 1
-
-    if x_pos < -bg_width:
-        x_pos = 0  
-
-    # Add other game elements (sprites, text, etc.) here
-    jack.flappyBird(DISPLAYSURF)
+class Skins:
+  def __init__(self,DISPLAYSURF):
+    self.DISPLAYSURF = DISPLAYSURF
+    image_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'images')  
+    background_path = os.path.join(image_folder, 'ground.jpg')
+    self.background = pygame.image.load(background_path).convert()
+    self.background = pygame.transform.smoothscale(self.background, self.DISPLAYSURF.get_size())
+    self.bg_width = self.background.get_width()
+    self.x_pos = 0
 
 
+
+  def display_background(self):
+      # Blit the self.background image
+      self.DISPLAYSURF.blit(self.background, (self.x_pos, 0))
+      self.DISPLAYSURF.blit(self.background, (self.x_pos + self.bg_width,0))
   
-    pygame.display.update()
-
-    clock.tick(80)
+      self.x_pos -= 1
+  
+      if self.x_pos < -self.bg_width:
+          self.x_pos = 0  
