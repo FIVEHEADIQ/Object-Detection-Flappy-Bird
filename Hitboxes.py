934f4a613d2d5e4283948f9c38dcbe03b0b7ebc0
TheSpicy0reo <114953521+TheSpicy0reo@users.noreply.github.com>
2024-01-16 18:14:46 -0500

    Update Hitboxes.py

--- Hitboxes.py
+++ Hitboxes.py
@@ -1,3 +1,68 @@
+# import Model_Pygame
+# import pygame
+# import random
+
+# class Game_play:
+#     def __init__(self):
+#         self.speed = 20
+#         self.pipe_width = 45
+#         self.pipe_x = 720 # Change, get resolution
+#         self.pipe_height = 720
+#         self.pipe_gap = 100
+#         self.pipe_gap_y = 140
+
+#         self.bird_x = 200
+
+#         self.score = 0
+
+#         self.color = (0, 255, 6)
+    
+#     def bird_hitbox(self, screen, model: Model_Pygame.Model):
+#         self.bird_y = model.get_y_pos() + 120
+#         pygame.draw.circle(screen, self.color, (self.bird_x, self.bird_y), 15) # Color OG: 0, 0, 6
+
+#     def pipe_hitboxes(self, screen):
+#         if self.pipe_x < -self.pipe_width: 
+#             self.pipe_x = 720 # Change
+#             self.pipe_gap_y = random.randint(140, 480 - self.pipe_gap - 20)
+#             self.passed_pipe = False
+
+#         # Upper pipe
+#         # pipe_x = screen.get_width()
+#         self.upper_pipe_y = self.pipe_gap_y - self.pipe_height
+
+#         pygame.draw.rect(screen, self.color, (self.pipe_x, self.upper_pipe_y, self.pipe_width, self.pipe_height))
+
+#         # Lower pipe
+#         self.lower_pipe_y = self.pipe_gap_y + self.pipe_gap
+
+#         pygame.draw.rect(screen, self.color, (self.pipe_x, self.lower_pipe_y, self.pipe_width, self.pipe_height))
+
+#         self.pipe_x -= self.speed
+
+
+#     def collision_x(self):
+#         return self.pipe_x <= self.bird_x - 15 <= self.pipe_x + self.pipe_width or self.pipe_x <= self.bird_x + 15 <= self.pipe_x + self.pipe_width
+    
+#     def collision_y(self):
+#         return not (self.pipe_gap_y <= self.bird_y - 15 <= self.lower_pipe_y) or not (self.pipe_gap_y <= self.bird_y + 15 <= self.lower_pipe_y)
+
+
+#     def collision(self):
+#         return self.collision_x() and self.collision_y()
+    
+
+#     def track_score(self):
+#         if self.passed_pipe == False:
+#             if self.pipe_x + self.pipe_width/2 <= self.bird_x:
+#                 self.score += 1
+#                 self.passed_pipe = True
+#                 print(self.score)
+
+#     def display_score(self):
+#         pass
+
+
 import Model_Pygame
 import pygame
 import random
@@ -11,6 +76,8 @@ class Game_play:
         self.pipe_nums = pipe_nums
         self.bird_x = 200
         self.num_resetted = 0
+        self.pipe_x_list = []
+        self.pipe_gap_y_list = []
         self.is_alive = True
 
         self.score = 0
@@ -22,16 +89,24 @@ class Game_play:
         pygame.draw.circle(screen, self.color, (self.bird_x, self.bird_y), 15) # Color OG: 0, 0, 6
 
     def pipe_hitboxes(self, screen):
-        for i in range(0, self.pipe_nums):
-            pipe_x: int;
+        for i in range(0, self.pipe_nums + 1):
             if self.num_resetted <= self.pipe_nums:
                 pipe_gap_y = random.randint(140, 480 - self.pipe_gap - 20)
-                pipe_x = 720 + 180 * self.pipe_nums #figure out a way to run this only once
+                pipe_x = 720 + self.pipe_width * i + 720/(self.pipe_nums + 1) * i #figure out a way to run this only once
                 self.num_resetted += 1
+                self.pipe_x_list.append(pipe_x)
+                self.pipe_gap_y_list.append(pipe_gap_y)
+
+            pipe_x = self.pipe_x_list[i]
+            # pipe_gap_y = self.pipe_gap_y_list[i]
+
             if pipe_x < -self.pipe_width: 
-                pipe_x = 720 # Change
+                pipe_x += 720 + (self.pipe_width + 1) * (self.pipe_nums + 1)
                 pipe_gap_y = random.randint(140, 480 - self.pipe_gap - 20)
+                self.pipe_gap_y_list[i] = pipe_gap_y
                 passed_pipe = False
+
+            pipe_gap_y = self.pipe_gap_y_list[i]
 
             # Upper pipe
             # pipe_x = screen.get_width()
@@ -46,8 +121,9 @@ class Game_play:
 
             
             if self.collision(pipe_x, self.bird_x, self.pipe_width, pipe_gap_y, self.bird_y, lower_pipe_y):
-                is_alive = False
+                self.is_alive = False
             pipe_x -= self.speed
+            self.pipe_x_list[i] = pipe_x
 
     def check_if_alive(self):
         return self.is_alive
