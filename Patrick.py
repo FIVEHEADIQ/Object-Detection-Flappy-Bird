1388050d0e8c37c4c43850281e5561d0166d1110
FIVEHEADIQ <100172251+FIVEHEADIQ@users.noreply.github.com>
2024-01-18 00:28:41 -0500

    Update Patrick.py

--- Patrick.py
+++ Patrick.py
@@ -5,134 +5,150 @@ https://www.programiz.com/python-programming/docstrings
 """
 
 import Model_Pygame
-import cv2
 import pygame
 import pygame.camera
 
-cam_started = False
-
-class Camera(): # Make all the methods static, remove need for camera object?
-    """A class to initialize a camera
+class Camera():
+    """Class which initializes camera in Pygame GUI
+    
+    Attributes
+    ----------
+    None
+    
+    Methods
+    -------
+    __init__():
+        Constructor for Camera Object
+    
+    get_cameras():
+        Gettor method users Cameras
+        
+    start_camera(selected_camera_index):
+        Starts camera using user selected Camera
+        
+    get_model():
+        Getter method for yolov8 model from Model_pygame.py
+        
+    stop_camera():
+        Stops Camera
+        
+    refresh_camera_list():
+        Loads list of available cameras user has
     """
     def __init__(self):
-        """Constructor
+        """Initalizes Camera object
         """
         pygame.camera.init()
         self.refresh_camera_list()
+        self.cam_started = False
+        
     def get_cameras(self):
-        # self.cameras = pygame.camera.list_cameras() 
-        # print(self.cameras) # Testing
+        """Gettor method for all cameras user has
+
+        Returns:
+            List[str]: Cameras user has
+        """
         return self.cameras
+    
     def start_camera(self, selected_camera_index):
-        global cam_started
-        if not cam_started:
-            # print(selected_camera_index) # Getting -1 RESOLVED
+        """Starts camera based on selected camera from user
+
+        Parameters:
+                selected_camera_index (int): Selected camera of user
+            
+        """
+        if not self.cam_started:
             self.model = Model_Pygame.Model(selected_camera_index)
-            cam_started = True
+            self.cam_started = True
+            
     def get_model(self):
         return self.model
+    
     def stop_camera(self):
         """
         """
-        global cam_started
-        if cam_started:
-            cam_started = False
+        if self.cam_started:
+            self.cam_started = False
             self.model.stop()
+            
     def refresh_camera_list(self):
         """Loads a list of all available cameras
         """
         self.cameras = pygame.camera.list_cameras()
 
 class Music: 
-    """Class for playing arcade style music while the user is in thee game
+    """Class for playing music while the user is in the menu, or any of the game difficulties
+    
+    Attributes
+    ----------
+    playlist : list[str]
+        List of files for each song
+    music_played : bool
+        Boolean value which represents if music is currently playing
+    music_stopped : bool
+        Boolean value which represents if music is currently stopped
+        
+    Methods
+    -------
+    __init__():
+        Constructor which initializes the music object
+    
+    play(index, loop=-1):
+        Start playing music, repeats if music is ended
+        
+    stop():
+        Stops music
+        
+    set_volume(volume):
+        Change volume of music, 0.0 for minimum, 1.0 for maximum
+        
+    get_volume():
+        Getter method for current volume
     """
     
     def __init__(self):
-        """
-        Constructor which initalizes the class with a given file
-
-        Parameters:
-        music_file (str): Path in files to the music file
+        """Constructor for Music Object
         """
         pygame.mixer.init()
         self.playlist = ["22-01. Super Smash Bros. Brawl Main Theme.mp3", "01 Title Theme.mp3", "1-17. Accumula Town.mp3", "4-12. Battle! (Champion).mp3", "645631_Lunar-Abyss.mp3"]
         self.music_played = False
         self.music_stopped = False
 
-    # def add_song(self, music_file):
-    #     """
-    #     Add a song to the playlist.
-
-    #     Parameters:
-    #     - music_file (str): The path to the music file.
-    #     """
-    #     self.playlist.append(music_file)
-    #     print(self.playlist)
-
     def play(self, index, loop=-1):
         """
-        Start playing the music.
+        Starts music
 
         Parameters:
-        loop (int): Number of times to loop the music. Set to -1 for infinite loop.
+        index (int): Indice of current song in the playlist
+        loop (int): Number of times to loop the music, currently looping infinitely.
         """
         if not self.music_played:
             pygame.mixer.music.load(self.playlist[index])
             pygame.mixer.music.play(loop)
             self.music_played = True
         
-    # def next_song(self):
-    #     """Switch to the next song in the playlist."""
-    #     self.stop()
-    #     self.current_song_index = (self.current_song_index + 1) % len(self.playlist)
-    #     self.play()
-        
-    def pause(self):
-        """Pause the currently playing music."""
-        pygame.mixer.music.pause()
-
-    def unpause(self):
-        """Unpause the currently paused music."""
-        pygame.mixer.music.unpause()
-
     def stop(self):
-        """Stop playing the music."""
+        """Stops music
+        """
         if not self.music_stopped:
             pygame.mixer.music.unload()
             pygame.mixer.music.stop()
-            # self.music_stopped = True
             self.music_played = False
-
-    # def reset(self):
-    #     self.music_stopped = True
-    #     pygame.mixer.music.unload()
 
     def set_volume(self, volume):
         """
-        Set the volume level of the music.
+        Setter method for volume of music
 
         Parameters:
-        volume (float): Volume level (0.0 to 1.0).
+        volume (float): Volume level (0.0 to 1.0)
         """
         pygame.mixer.music.set_volume(volume)
 
     def get_volume(self):
         """
-        Get the current volume level of the music.
+        Getter method for volume of music
 
         Returns:
-        float: The current volume level (0.0 to 1.0).
+        float: The current volume level (0.0 to 1.0)
         """
         return pygame.mixer.music.get_volume()
-    
-    # def load_playlist(self, playlist):
-    #     """
-    #     Load a playlist of music tracks.
-
-    #     Args:
-    #         playlist (list): List of paths to music files."""
-    #     self.playlist = playlist
-    #     pygame.mixer.music.load(self.playlist)
-
-    # def get_playlist(self):
-    #     return self.playlist
