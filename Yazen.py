ef15b6efed4e4d720e01c9302b9554f4faff28bb
TheSpicy0reo <114953521+TheSpicy0reo@users.noreply.github.com>
2024-01-18 01:34:27 -0500

    Update Yazen.py

--- Yazen.py
+++ Yazen.py
@@ -31,12 +31,12 @@ class Skins:
     Display the background image on the display surface in a shifting pattern.
     """
       # Blit the self.background image
-      self.DISPLAYSURF.blit(self.background, (self.x_pos, 0))
-      self.DISPLAYSURF.blit(self.background, (self.x_pos + self.bg_width,0))
+    self.DISPLAYSURF.blit(self.background, (self.x_pos, 0))
+    self.DISPLAYSURF.blit(self.background, (self.x_pos + self.bg_width,0))
     """
     Update the x position of the object and reset it to 0 if it goes beyond the width of the background.
     """
-      self.x_pos -= 1
+    self.x_pos -= 1
   
-      if self.x_pos < -self.bg_width:
-          self.x_pos = 0  
+    if self.x_pos < -self.bg_width:
+        self.x_pos = 0  
