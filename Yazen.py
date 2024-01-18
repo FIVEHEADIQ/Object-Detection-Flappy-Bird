d4af6640ce3f93fe63e18ed1dab4aace665c540a
TheSpicy0reo <114953521+TheSpicy0reo@users.noreply.github.com>
2024-01-17 21:05:21 -0500

    Update Yazen.py

--- Yazen.py
+++ Yazen.py
@@ -5,14 +5,16 @@ import os
 class Skins:
   def __init__(self,DISPLAYSURF):
     self.DISPLAYSURF = DISPLAYSURF
-    image_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'images')  
-    background_path = os.path.join(image_folder, 'ground.jpg')
-    self.background = pygame.image.load(background_path).convert()
-    self.background = pygame.transform.smoothscale(self.background, self.DISPLAYSURF.get_size())
-    self.bg_width = self.background.get_width()
-    self.x_pos = 0
+    # image_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'images')  
+    # background_path = os.path.join(image_folder, 'ground.jpg')
+    # self.background = pygame.image.load(background_path).convert()
+    # self.background = pygame.transform.smoothscale(self.background, self.DISPLAYSURF.get_size())
+    # self.bg_width = self.background.get_width()
+    # self.x_pos = 0
+    self.bird_list = ['flappy bird mans/Blue Man.png', 'flappy bird mans/Green Hat Man.png', 'flappy bird mans/Pink Man.png', 'flappy bird mans/Pixel Blue Man', 'flappy bird mans/Pixel Yellow Man.png', 'flappy bird mans/Yellow Hat Man.png']
 
-
+  def get_bird_list(self):
+     return self.bird_list
 
   def display_background(self):
       # Blit the self.background image
